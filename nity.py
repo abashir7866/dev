9a3649f5c18a087f1d2ad15635e9a55304950694
abashir7866 <79613256+abashir7866@users.noreply.github.com>
2022-02-23 15:49:27 -0500

    no typo ty rinkerbel bb

--- nity.py
+++ nity.py
@@ -1,4 +1,4 @@
-@Ghost.command(name="nihty", description="nighty user = retard", usage="nighty")
+@Ghost.command(name="nighty", description="nighty user = retard", usage="nighty")
 async def nighty(ctx):
     await ctx.send("https://media.discordapp.net/attachments/934555193838813214/946139744239239238/image0-16.gif")
     
